22201f0fe54ae7862bc3367d66a7d84e7fe1ce4d
22Krunal <96976343+22Krunal@users.noreply.github.com>
2023-02-05 22:22:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,9 @@ app.use('/student',studentRouter);
 app.use('/company',companyRouter);
 app.use('/job',JobRouter);
 
-if (app.get("env") === "production") {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
- }
+// if (app.get("env") === "production") {
+//     app.use(enforce.HTTPS({ trustProtoHeader: true }));
+//  }
 
 app.listen(5000,()=>{
     console.log('Server is Listing at port 5000');
